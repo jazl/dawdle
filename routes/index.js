e5dfd1dcb83cf6a2e64c874ed253e1a5ac9933b9
jazl <alan.laya@gmail.com>
2018-04-07 16:00:02 -0700

    add admin, books and a bunch of other stuff

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var { MongoClient } = require('mongodb');
 
 const title = process.env.TITLE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title });
+  const url = process.env.URL;
+  const user = process.env.USER;
+  const password = process.env.PASSWORD;
+  const dbname = 'library';
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url, {auth:{user,password}});
+      const db = client.db(dbname);
+      const response = await db.collection('books').find().toArray();
+      res.json(response);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }());
 });
 
 module.exports = router;
